6e3e606702c793c6264c74b3f45f7014cfbc39b4
snoopysnake <chiang4@gmail.com>
2018-10-09 13:24:05 -0400

    updated

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+var irl = false;
+
 window.onload = function setup() {
 	setUpBubble('abt', 'about me');
 	setUpBubble('port', 'my portfolio');
 	setUpBubble('cv', 'my resume');
+	hoverOver();
 }
 
 function setUpBubble(id, text) {
@@ -17,5 +20,39 @@ function setUpBubble(id, text) {
 		var message = document.getElementById('msg');
 		message.innerHTML = '';
 		bubble.style.display = 'none';
+	}
+}
+
+function hoverOver() {
+	var me = document.getElementById('me');
+	// me.onmouseover = function() {
+	// 	if (!irl) {
+	// 		me.src = 'img/meirl.png';
+	// 		irl = true;
+	// 	}
+	// 	else {
+	// 		me.src = 'img/me5.png';
+	// 		irl = false;
+	// 	}
+	// }
+	// me.onclick = function() {
+	// 	if (!irl) {
+	// 		me.src = 'img/meirl.png';
+	// 		irl = true;
+	// 	}
+	// 	else {
+	// 		me.src = 'img/me5.png';
+	// 		irl = false;
+	// 	}
+	// }
+	me.onclick = function() {
+		if (!irl) {
+			me.src = 'img/meirl.png';
+			irl = true;
+		}
+		else {
+			me.src = 'img/me5.png';
+			irl = false;
+		}
 	}
 }
