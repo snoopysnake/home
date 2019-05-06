92265f6990f4225a1be47b750ea6cce3e24a00c7
snoopysnake <chiang4@gmail.com>
2019-05-05 22:04:29 -0400

    fixed some typos

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const hamburger = document.getElementById('menuToggle');
 const checkbox = document.querySelector('#menuToggle input');
 
 document.body.addEventListener('click', function(e) {
-	console.log(e.target.parentNode);
 	if (e.target.parentNode != menu && e.target.parentNode != hamburger) {
 		if (checkbox.checked) {
 			checkbox.click();
